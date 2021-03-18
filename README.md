3d2325b29e9afb69b1ebdf602579ebd5e354164d
David Strauss <david@davidstrauss.net>
2021-03-18 07:48:03 -0700

    Roughly working

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# foss-devcontainer
+# FOSS .devcontainer
+
+A support framework for using `.devcontainer` on Linux desktops. Let's start
+with Buildah + Podman + Builder, but the project is open to contributions for
+other IDE integrations.
+
+## On Fedora Silverblue
+
+### Setup
+
+1. Install GNOME Builder:
+
+       flatpak install flathub org.gnome.Builder
+
+1. Set up a [Toolbox](https://docs.fedoraproject.org/en-US/fedora-silverblue/toolbox/).
+
+       toolbox create
+
+### Usage
+
+1. Open the "Switch Surface" menu (top left).
+1.
+
+## Resources
+
+https://github.com/containers/buildah/issues/2404
---
+++ makeitso.py
@@ -0,0 +1,72 @@
+import pathlib
+import string
+import json
+import os
+import subprocess
+import logging
+import sys
+import configparser
+
+base_path = os.path.dirname(os.path.realpath(__file__))
+buildconfig_tpl_path = os.path.join(base_path, "buildconfig.template")
+buildconfig_tpl_str = pathlib.Path(buildconfig_tpl_path).read_text()
+buildconfig_tpl = string.Template(buildconfig_tpl_str)
+
+project_path = os.getcwd()  # Make configurable?
+project_name = pathlib.Path(project_path).name.strip()
+
+
+def init_logger():
+    # From https://stackoverflow.com/a/46065766
+    logger = logging.getLogger()
+    h = logging.StreamHandler(sys.stdout)
+    h.flush = sys.stdout.flush
+    logger.addHandler(h)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+
+logger = init_logger()
+
+
+def run(cmd, buildah_unshare=False):
+    if buildah_unshare:
+        cmd = ["buildah", "unshare"] + cmd
+    logger.info("Command: {}".format(cmd))
+    cproc = subprocess.run(cmd, check=True, capture_output=True)
+    output = cproc.stdout.strip()
+    logger.info("Output: {}".format(output))
+    return output
+
+
+contained_id = run(["buildah", "from", "scratch"])
+scratch_mnt = run(["buildah", "mount", contained_id], True)
+
+devc_dir = os.path.join(os.getcwd(), ".devcontainer")
+devc_config_path = os.path.join(devc_dir, "devcontainer.json")
+
+devc_config_json = ""
+with open(devc_config_path, "r") as devc_config_fp:
+    for line in devc_config_fp.readlines():
+        if not line.strip().startswith("//"):
+            devc_config_json += line + "\n"
+devc_config = json.loads(devc_config_json)
+
+container_build_filename = devc_config["build"]["dockerfile"]
+container_build_path = os.path.join(devc_dir, container_build_filename)
+
+pb_output = run(
+    ["podman", "build", "-t", project_name, "-f", container_build_path, "."]
+)  # --build-arg
+image_id = pb_output.splitlines()[-1].decode("utf-8").strip()
+
+run(["podman", "rm", "--ignore", project_name])
+run(["podman", "run", "-d", "--name={}".format(project_name), image_id])
+
+bldcfg = buildconfig_tpl.substitute(
+    containerid=project_name, homedir=pathlib.Path.home(), project=project_name
+)
+config = configparser.ConfigParser()
+config.read_string(bldcfg)
+with open(".buildconfig", "w") as configfile:
+    config.write(configfile)
